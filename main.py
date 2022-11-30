038d6e883d31379f2c62c8576f31e68cc9e4c4a2
Norm×Alpha.vg <87635928+NormVg@users.noreply.github.com>
2022-11-30 12:19:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import uvicorn
+from ml import ml_app
+app = FastAPI()
+
+@app.get("/{que}")
+async def root(que):
+    ans = ml_app(que)
+    return ans
+#
+# @app.get("/app")
+# async def app():
+#     ans = ml_app("parents of Bruce Wayne ?")
+#     return ans
+
+
+if __name__ =="__main__":
+    uvicorn.run("main:app",reload=True)
